afb7daa2f6e3b8dd794160e447756a0172166c16
Sushmita Yadav <yadavsushmita293@gmail.com>
2023-12-26 14:01:39 +0530

    adding all the files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import RandomPasswordGenerator from './RandomPasswordGenerator'
+function App() {
+
+  return (
+    <>
+      <RandomPasswordGenerator/>
+    </>
+  )
+}
+
+export default App
